4fdf7fc2d7725f118aa8deea2b96ca63be46e935
Simon Let <simon.a.let@gmail.com>
2018-03-28 01:42:33 +0200

    Draft

---
+++ example_config.sh
@@ -0,0 +1,6 @@
+# git-meta-status config
+git_meta_remote='git@gitlab.com:curusarn/git-meta-status-data.git'
+git_meta_path=~/.cache/git-meta-status
+git_dir_path=~/git
+device_id=whatever
+track_dotfiles=true
