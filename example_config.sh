69266b3d5d47c073b6bd1e6d26b29aeae57f45eb
Šimon Let <simon.a.let@gmail.com>
2018-04-07 00:12:35 +0200

    Delete example_config.sh

--- example_config.sh
+++
@@ -1,6 +0,0 @@
-# git-meta-status config
-git_meta_remote='git@gitlab.com:curusarn/git-meta-status-data.git'
-git_meta_path=~/.cache/git-meta-status
-git_dir_path=~/git
-device_id=whatever
-track_dotfiles=true
